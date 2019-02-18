a9e1bb2913a93859ab19ca5cc1e044281589491a
Ronnie Ho <ronnieyho@gmail.com>
2019-02-17 19:54:39 -0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+// Import the dependencies and necessary modules
+import Vue from 'vue';
+import App from './App.vue';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import Widgets from 'fusioncharts/fusioncharts.widgets';
+import PowerCharts from 'fusioncharts/fusioncharts.powercharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import VueFusionCharts from 'vue-fusioncharts';
 
-Vue.config.productionTip = false
+// Resolve the dependencies
+Charts(FusionCharts);
+PowerCharts(FusionCharts);
+Widgets(FusionCharts);
+FusionTheme(FusionCharts);
 
+// Globally register the components for project-wide use
+Vue.use(VueFusionCharts, FusionCharts);
+
+// Instantiate the Vue instance that controls the application
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+ el: '#app',
+ render: h => h(App)
+})
